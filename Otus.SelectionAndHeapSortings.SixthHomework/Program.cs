0aca664c15de0198ebb15b858612f7abe3dc7f3c
Ekaterina Korotkova <e.korotkova.2013@yandex.ru>
2022-05-27 21:57:36 +0300

    initial commit

---
+++ Otus.SelectionAndHeapSortings.SixthHomework/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Otus.SelectionAndHeapSortings.SixthHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
