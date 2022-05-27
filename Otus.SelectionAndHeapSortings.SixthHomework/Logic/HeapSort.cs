5c5d9749ec35a3dc6efd540714ee9905ed9d8582
Ekaterina Korotkova <e.korotkova.2013@yandex.ru>
2022-05-28 00:30:44 +0300

    implemented heap sort

---
+++ Otus.SelectionAndHeapSortings.SixthHomework/Logic/HeapSort.cs
@@ -0,0 +1,95 @@
+﻿namespace Otus.SelectionAndHeapSortings.SixthHomework.Logic
+{
+    public class HeapSort
+    {
+        private int[] _array;
+
+
+        public HeapSort(int[] array)
+        {
+            _array = array;
+        }
+
+
+        public int[] Run()
+        {
+            //начальное становление пирамиды
+            for (var root = _array.Length / 2 - 1; root >= 0; root--)
+            {
+                Heapify(root, _array.Length);
+            }
+
+            for (var i = _array.Length - 1; i >= 0; i--)
+            {
+                //переносим МАХ (который оказался на вершине кучи - [0]) в конец массива
+                Swap(0, i);
+
+                //перестраиваем кучу, с учетом исключения MAX (обрезали длину массива)
+                Heapify(0, i);
+            }
+
+            return _array;
+        }
+
+
+        #region Support methods
+
+        /// <summary>
+        /// Перестраиваем дерево так, чтобы максимальный элемент был наверху
+        /// </summary>
+        private void Heapify(int rootIndex, int size)
+        {
+            var leftIndex = 2 * rootIndex + 1;
+            var rightIndex = 2 * rootIndex + 2;
+
+            var maxIndexInSubTree = GetIndexOfMaxElementInSubTree(rootIndex, leftIndex, rightIndex, size);
+            if (maxIndexInSubTree == rootIndex)
+                return;
+            
+            //поднимаем левого/правого чайлда на место парента
+            //бывший парент спускается на уровень левого/правого чайлда
+            Swap(maxIndexInSubTree, rootIndex);
+
+            //после того, как бывший парент опустился на место чайлда
+            //нужно проверить/перестроить "внуков"
+            Heapify(maxIndexInSubTree, size);
+        }
+
+        private int GetIndexOfMaxElementInSubTree(int rootIndex, int leftIndex, int rightIndex, int size)
+        {
+            bool ExceedLeftBorder() => leftIndex >= size;
+            bool ExceedRightBorder() => rightIndex >= size;
+
+            int maxIndex = rootIndex;
+            
+            if (!ExceedLeftBorder())
+            {
+                if (_array[leftIndex] > _array[rootIndex])
+                {
+                    if (ExceedRightBorder() || _array[leftIndex] > _array[rightIndex])
+                        maxIndex = leftIndex;
+                }
+            }
+
+            if (!ExceedRightBorder())
+            {
+                if (_array[rightIndex] > _array[rootIndex])
+                {
+                    if (ExceedLeftBorder() || _array[rightIndex] > _array[leftIndex])
+                        maxIndex = rightIndex;
+                }
+            }
+
+            return maxIndex;
+        }
+
+        private void Swap(int x, int y)
+        {
+            var tmp = _array[x];
+            _array[x] = _array[y];
+            _array[y] = tmp;
+        }
+
+        #endregion
+    }
+}
--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -5,11 +5,11 @@ namespace Tests
 {
     public class Tests
     {
-        //private int[] _initialArray = { 32, 95, 16, 82, 24, 66, 35, 19, 75, 54, 40, 43, 93, 68 };
-        //private int[] _sortedArray = { 16, 19, 24, 32, 35, 40, 43, 54, 66, 68, 75, 82, 93, 95 };
+        private int[] _initialArray = { 32, 95, 16, 82, 24, 66, 35, 19, 75, 54, 40, 43, 93, 68 };
+        private int[] _sortedArray = { 16, 19, 24, 32, 35, 40, 43, 54, 66, 68, 75, 82, 93, 95 };
 
-        private int[] _initialArray = { 8, 2, 3, 5 };
-        private int[] _sortedArray = { 2, 3, 5, 8 };
+        //private int[] _initialArray = { 8, 2, 3, 5 };
+        //private int[] _sortedArray = { 2, 3, 5, 8 };
 
 
         [Test]
@@ -18,6 +18,16 @@ namespace Tests
             var sorter = new SelectionSort();
 
             var sortedArray = sorter.Run(GetInitialArray());
+
+            CollectionAssert.AreEqual(_sortedArray, sortedArray);
+        }
+
+        [Test]
+        public void Can_Sort_Via_Heap_Sorting()
+        {
+            var sorter = new HeapSort(GetInitialArray());
+
+            var sortedArray = sorter.Run();
 
             CollectionAssert.AreEqual(_sortedArray, sortedArray);
         }
